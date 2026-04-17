af1b06ecf55382ffe314a620da86a1845612e594
yahyaobeid <yobeid@sfsu.edu>
2026-04-16 22:05:21 -0700

    chore: add custom domain configuration

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/agarwood/',
+  base: '/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
